63fd4cf03b128a33b9bfcb6d9c8c2500003fddb4
Zu34 <gmathi091@gmail.com>
2025-12-17 07:48:58 -0800

    menu

--- src/app/pages/menuband/menuband.component.ts
+++ src/app/pages/menuband/menuband.component.ts
@@ -1,18 +1,54 @@
 // src/app/pages/menuband/menuband.component.ts
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FoodishService } from '../../services/foodiesh.service';
 
 @Component({
   selector: 'app-menuband',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './menuband.component.html'
+  templateUrl: './menuband.component.html',
+  styleUrls: ['./menuband.component.scss']
 })
-export class MenubandComponent {   // <-- rename here if you want this name
-  menuItems = [
-    { title: 'Menu Vertical', img: 'assets/images/menu-1.jpg' },
-    { title: 'Menu Small Images', img: 'assets/images/menu-2.jpg' },
-    { title: 'Menu Big Images', img: 'assets/images/menu-3.jpg' },
-    { title: 'Menu Simple', img: 'assets/images/menu-4.jpg' }
+export class MenubandComponent implements OnInit {
+  @Input() provider: 'mealdb' = 'mealdb';
+
+  // VALID MealDB categories only
+  categories = [
+    { key: 'Beef', title: 'Menu Vertical' },
+    { key: 'Chicken', title: 'Menu Small Images' },
+    { key: 'Dessert', title: 'Menu Big Images' }
   ];
+
+  menuItems: {
+    title: string;
+    img: string | null;
+    loading: boolean;
+    error: boolean;
+  }[] = [];
+
+  constructor(private foodish: FoodishService) {}
+
+  async ngOnInit() {
+    // Initialize UI state
+    this.menuItems = this.categories.map(c => ({
+      title: c.title,
+      img: null,
+      loading: true,
+      error: false
+    }));
+
+    await Promise.all(
+      this.categories.map(async (cat, index) => {
+        try {
+          const img = await this.foodish.getRandomImage(cat.key);
+          this.menuItems[index].img = img;
+        } catch {
+          this.menuItems[index].error = true;
+        } finally {
+          this.menuItems[index].loading = false;
+        }
+      })
+    );
+  }
 }
